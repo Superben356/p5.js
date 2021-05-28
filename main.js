1e8f69b1d91402afdc6631fab8180e3bcd4b2e37
Superben356 <75051639+Superben356@users.noreply.github.com>
2021-05-27 21:11:32 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+function preload(){
+
+}
+
+function setup(){
+    canvas = createCanvas(640,480);
+    canvas.position(110,250);
+}
+
+
+function draw(){
+    rect(20, 20, 600 ,440, 20);
+    image(0 , 0,640, 480);
+}
